fca6071fb1528a430282ae01f0804d4c17a0181e
andexist <andexist@gmail.com>
2020-01-17 22:42:08 +0200

    payment providers fix

--- payment/src/app/Http/Controllers/Requests/CreatePaymentRequest.php
+++ payment/src/app/Http/Controllers/Requests/CreatePaymentRequest.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Requests;
 
-use App\Payment;
+use App\Account;
 use Illuminate\Foundation\Http\FormRequest;
 
 /**
@@ -19,8 +19,8 @@ class CreatePaymentRequest extends FormRequest
     public function rules()
     {
         return [
-            'paymentProvider' => 'required|in:' . implode(",", Payment::PAYMENT_PROVIDER),
             'accountId' => 'required|exists:accounts,id',
+            'currency' => 'required|in:' . implode(",", Account::AVAILABLE_CURRENCIES),
             'amount' => 'required',
             'receiverAccount' => 'required',
             'receiverName' => 'required',
